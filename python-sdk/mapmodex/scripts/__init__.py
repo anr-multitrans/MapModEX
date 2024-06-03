bef89290df77878a25900825e3c06d05b4aa6190
YANG Li <lillyyang2019@outlook.com>
2024-06-03 04:43:36 +0200

    style: rename files

--- python-sdk/mapmodex/scripts/__init__.py
+++ python-sdk/mapmodex/scripts/__init__.py
@@ -1,4 +1,4 @@
-from .custom_av2_map_converter_copy import create_av2_infos_mp
-from .custom_nusc_map_converter_copy import create_nuscenes_infos
+from .av2_reading import create_av2_infos_mp
+from .nuscenes_reading import create_nuscenes_infos
 from .peturbation import PerturbParameters
 from .data_reading import GetMapLayerGeom
